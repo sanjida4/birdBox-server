e9990c662b1142fc4762426d36d5f798bbcdeb8d
Promy <promysanjida@gmail.com>
2021-04-01 19:07:21 -0700

    done

--- index.js
+++ index.js
@@ -18,27 +18,27 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     console.log("connection error", err);
-    const birdsCollection = client.db("birdBox").collection("birds");
+    const productsCollection = client.db("birdBox").collection("birds");
     const ordersCollection = client.db("birdBox").collection("orders");
 
-  app.get("/birds", (req, res) => {
-      birdsCollection.find({})
+  app.get("/products", (req, res) => {
+    productsCollection.find({})
       .toArray((err, items) => {
          res.send(items);
       })
   })
   
-  app.post('/addBird', (req, res) => {
-    const newBird = req.body;
-    console.log("adding new bird: ", newBird);
-    birdsCollection.insertOne(newBird)
+  app.post('/addProduct', (req, res) => {
+    const newProduct = req.body;
+    console.log("adding new bird: ", newProduct);
+    productsCollection.insertOne(newProduct)
     .then((result) => {
         console.log( 'inserted count', result.insertedCount);
         res.send(result.insertedCount)
     })
   })
   
-  app.get('/bird/:key', (req, res) => {
+  app.get('/product/:key', (req, res) => {
     productsCollection.find({key: req.params.key})
     .toArray( (err, documents) => {
         res.send(documents[0]);
@@ -64,4 +64,4 @@ app.post('/addOrder', (req, res) => {
 });
 
 
-app.listen(port)
+app.listen(port);
